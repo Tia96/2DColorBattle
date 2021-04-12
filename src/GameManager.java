7c0901516545476514a77a79d2bb930fa4736da7
Tia96 <i.tai9996@hotmail.co.jp>
2021-04-13 01:25:53 +0900

    GameManager追加

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,2 +1,31 @@
 public class GameManager {
+    private static final GameManager INSTANCE = new GameManager();
+    private boolean gameLoop;
+    private double targetFpsTime = 1000 / 30.0;
+    private long startTime;
+
+    private GameManager() {}
+
+    public static GameManager getInstance() {
+        INSTANCE.gameLoop = true;
+        return INSTANCE;
+    }
+
+    public void init() {
+
+    }
+//->ready "ID" ready "ID"? OK. OK.
+    public void start() throws InterruptedException {
+        int frames = 0;
+        startTime = System.currentTimeMillis();
+        while (INSTANCE.gameLoop) {
+            long sT = System.currentTimeMillis();
+            ++frames;
+
+            long elapsedTime = System.currentTimeMillis() - sT;
+            if(elapsedTime < targetFpsTime) Thread.sleep((long) (targetFpsTime - elapsedTime));
+
+            System.out.println("fps: " + (double)frames / (System.currentTimeMillis() - startTime) * 1000);
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -3,29 +3,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static final int PORT = 60040;
+    public final int PORT = 60040;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket s = new ServerSocket(PORT);
-        System.out.println("Started: " + s);
-        try {
-            Socket socket = s.accept();
-            try {
+        Server server = new Server();
+        server.connect();
+    }
+
+    private void connect() throws IOException {
+        try (ServerSocket s = new ServerSocket(PORT)) {
+            System.out.println("Started: " + s);
+            try (Socket socket = s.accept()) {
                 System.out.println("Connection accepted: " + socket);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                while (true) {
-                    String str = in.readLine();
-                    if(str.equals("END")) break;
-                    System.out.println("Echoing: " + str);
-                    out.println(str);
-                }
-            } finally {
-                System.out.println("closeing...");
-                socket.close();
+                GameManager gameManager = GameManager.getInstance();
+//                    String str = in.readLine();
+//                    if(str.equals("END")) break;
+//                    System.out.println("Echoing: " + str);
+//                    out.println(str);
+                System.out.println("yes");
+                gameManager.init();
+                gameManager.start();
+                System.out.println("closing: " + socket);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } finally {
-            s.close();
         }
     }
 }
