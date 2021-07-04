c7ec079166de38c1d935397867e1cc5b538eb544
Tia96 <i.tai9996@hotmail.co.jp>
2021-07-05 00:47:56 +0900

    add properties

--- src/main/Network.java
+++ src/main/Network.java
@@ -6,21 +6,42 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class Network {
-    private static final int PORT = 60040;
+    private static final String SETTING_FILE_PATH = "resources/settings.properties";
+    private static final Properties properties = new Properties();
+
+    private static int PORT;
+    private static String HOSTNAME;
     public Socket socket;
     private BufferedReader in;
     private PrintWriter out;
-    private final Deque<String> messageBox = new ArrayDeque<>();
+    private final String[] messageBox;
+    private int index = 0;
+
+    Network() {
+        try {
+            properties.load(Files.newBufferedReader(Paths.get(SETTING_FILE_PATH)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PORT = Integer.parseInt(properties.getProperty("PORT", "60040"));
+        HOSTNAME = properties.getProperty("HOSTNAME", "localhost");
+
+        messageBox = new String[100];
+        for (int i = 0; i < 100; ++i) {
+            messageBox[i] = "";
+        }
+    }
 
     public Observable<String> connect() {
         return Observable.create(emitter -> {
             while (true) {
                 try {
-                    InetAddress addr = InetAddress.getByName("localhost");
+                    InetAddress addr = InetAddress.getByName(HOSTNAME);
                     socket = new Socket();
                     socket.connect(new InetSocketAddress(addr, PORT), 0);
                     in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -30,7 +51,7 @@ public class Network {
                     emitter.onNext(result);
                     break;
                 } catch (IOException e) {
-                    System.out.println("Connecting ...");
+                    System.out.println("Connecting to " + HOSTNAME + ": " + PORT);
                     try {
                         Thread.sleep(2000);
                     } catch (InterruptedException interruptedException) {
@@ -52,14 +73,17 @@ public class Network {
                     e.printStackTrace();
                 }
                 System.out.println("_in: " + str);
-                messageBox.push(str);
+                messageBox[index] = str;
+                index += 1;
+                if (index == 100) index = 0;
             }
         }).start();
     }
 
     public String getMessage() {
-        if (messageBox.size() == 0) return "";
-        return messageBox.getFirst();
+        int idx = index - 1;
+        if (idx < 0) idx += 100;
+        return messageBox[idx];
     }
 
     public void sendMessage(String str) {
